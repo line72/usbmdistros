d8fce83f9562fac38cb0f70b61b13e00569d4d6a
Marcus Dillavou <line72@line72.net>
2021-06-10 17:10:14 -0500

    Switched StoVoKor to pull from the json list of products rather than the rss feed, since the rss feed does not contain all products

--- libbmdistro/parsers/StoVoKor.py
+++ libbmdistro/parsers/StoVoKor.py
@@ -3,25 +3,33 @@
 #
 # Released under the GPLv3
 
-import feedparser
+import requests
+import urllib
 
 from .Parser import Parser
 from ..Product import Product
 
 class StoVoKor(Parser):
     def parse(self, db):
-        feed = feedparser.parse(self.feed)
+        r = requests.get(self.feed)
+        r.raise_for_status()
 
-        entries = map(lambda e: self.parseItem(db, e), feed.entries)
+        products = r.json()['products']
+        
+        entries = map(lambda e: self.parseItem(db, e), products)
         return list(entries)
 
     def parseItem(self, db, entry):
         artist, album = entry['title'].split(sep = '-', maxsplit = 1)
         artist = artist.strip()
         album = album.strip()
-        item_type = entry['s_type']
-        price = entry['s_variant']
+        item_type = entry['product_type']
+        price = entry['variants'][0]['price']
+
+        handle = entry['handle']
+        u = urllib.parse.urlparse(self.feed)
+        link = f'{u.scheme}://{u.netloc}/products/{handle}'
 
         album = db.get_album(artist, album, item_type)
         
-        return Product(album, self.store, entry['link'], price, Product.STOCK_UNKNOWN, -1)
+        return Product(album, self.store, link, price, Product.STOCK_UNKNOWN, -1)
